c97057ea0cf8ddc9354f831a02e8b33185c5310c
RuthramoorthyP <RuthramoorthyP@ASGLH-WL-12926.kingsway.asos.com>
2017-11-15 13:22:43 +0000

    added

--- src/services/fixer/fixtures.tsx
+++ src/services/fixer/fixtures.tsx
@@ -3,7 +3,7 @@ export const latestJson = `{
     "date": "2017-11-13",
     "rates": {       
         "CAD": 1.4929,
-        "USD": 1.065       
+        "USD": 1.1745       
     }
 }`;
 export const latestResponse: IFixerServiceResponse = JSON.parse(latestJson);
